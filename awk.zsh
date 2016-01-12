856d0faa794d0c55f2c2b7032dd1e792942dffcf
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-01-12 12:04:02 -0200

    removing merge files

--- awk.zsh
+++
@@ -1,2 +0,0 @@
-# print lines that match a criteria
-git status --short | awk '$1 == "AA"' {print $0}
--- file_manipulation.zsh
+++
@@ -1,5 +0,0 @@
-# find all dot files on home directory
-ls -la `find ~ -maxdepth=1 -type l`
-
-# show 20 biggest folders from current dir
-du -sm * | sort -nr | head -n 20
