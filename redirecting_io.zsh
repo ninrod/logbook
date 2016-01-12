58a0df21157907dd9d17c033fdc7e88c5ebfa5c2
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-01-12 11:46:15 -0200

    adding redirection tricks

---
+++ redirecting_io.zsh
@@ -0,0 +1,12 @@
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
+
+# Open STDOUT as $LOG_FILE file for read and write.
+exec 1<>$LOG_FILE
+
+# Redirect STDERR to STDOUT
+exec 2>&1
+
+echo "This line will appear in $LOG_FILE, not 'on screen'"
