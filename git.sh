e2a8425ae93f34fdb4eaa08ec39847d41131a32b
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-01-19 17:12:44 -0200

    adding comments

--- git.sh
+++ git.sh
@@ -1,11 +1,14 @@
-# showing what branch is tracking what remote
+#### showing what branch is tracking what remote
 git branch -vv (doubly verbose)
 
-### remote branch checkout.
+#### remote branch checkout.
 # here 'remote_branch_name' doesn't exist locally and matches only one name on the remote
 git checkout remote_branch_name
 # or, as an alternative
 git checkout -b localname origin/branchname
+# or, even more verbose:
+git branch --track experimental origin/experimental
+git checkout experimental
 
-# seeing what changes are comming from upstream (diff)
+#### seeing what changes are comming from upstream (diff)
 git diff master origin/master
