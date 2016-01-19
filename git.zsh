6e8125f6da32cc877f9a1198fa40fa0561d4d749
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-01-19 17:10:00 -0200

    git trick

--- git.zsh
+++ git.zsh
@@ -1,9 +1,11 @@
 # showing what branch is tracking what remote
 git branch -vv (doubly verbose)
 
-# remote branch checkout.
+### remote branch checkout.
 # here 'remote_branch_name' doesn't exist locally and matches only one name on the remote
 git checkout remote_branch_name
+# or, as an alternative
+git checkout -b localname origin/branchname
 
 # seeing what changes are comming from upstream (diff)
 git diff master origin/master
