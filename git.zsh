8e523f7a4c11d35ed93c206f62b07a21749ad0d3
ninrod <filipe.silva@gmail.com>
2016-01-18 23:16:58 -0200

    adding git trick

--- git.zsh
+++ git.zsh
@@ -1,2 +1,6 @@
 # showing what branch is tracking what remote
 git branch -vv (doubly verbose)
+
+# remote branch checkout.
+# here 'remote_branch_name' doesn't exist locally and matches only one name on the remote
+git checkout remote_branch_name
