49912b9d7ab29bf9345966193716e8d03f0064af
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2016-01-19 16:30:40 -0200

    git trick

--- git.zsh
+++ git.zsh
@@ -4,3 +4,6 @@ git branch -vv (doubly verbose)
 # remote branch checkout.
 # here 'remote_branch_name' doesn't exist locally and matches only one name on the remote
 git checkout remote_branch_name
+
+# seeing what changes are comming from upstream (diff)
+git diff master origin/master
